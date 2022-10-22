fc2e886d007c1e3f2bdd65380d49ce1a91cc636f
Steven Zhang2 <steven_zhang2@epam.com>
2022-10-23 01:17:59 +0800

    generate JWT token endpoint

--- src/main/java/com/example/jwtdemo/controller/JwtController.java
+++ src/main/java/com/example/jwtdemo/controller/JwtController.java
@@ -1,7 +1,15 @@
 package com.example.jwtdemo.controller;
 
 import com.example.jwtdemo.model.JwtRequest;
+import com.example.jwtdemo.model.JwtResponse;
+import com.example.jwtdemo.service.CustomUserDetailService;
+import com.example.jwtdemo.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/api")
 public class JwtController {
 
-    @PostMapping("/generateToken")
-    public ResponseEntity<> generateToken(@RequestBody JwtRequest request) {
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/generateToken")
+    public ResponseEntity<JwtResponse> generateToken(@RequestBody JwtRequest request) {
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
+        authenticationManager.authenticate(authToken);
+
+        UserDetails userDetails = customUserDetailService.loadUserByUsername(request.getUsername());
+        String token = jwtUtil.generateToken(userDetails);
+
+        JwtResponse jwtResponse = new JwtResponse(token);
+//        return ResponseEntity.ok(jwtResponse);
+        return new ResponseEntity<JwtResponse>(jwtResponse, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/example/jwtdemo/model/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.example.jwtdemo.model;
+
+public class JwtResponse {
+
+    private String token;
+
+    public JwtResponse() {
+    }
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+}
