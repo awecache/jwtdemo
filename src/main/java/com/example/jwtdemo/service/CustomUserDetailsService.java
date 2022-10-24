55252b799a5edc9115ad133a56fa830d7477ae94
Steven Zhang2 <steven_zhang2@epam.com>
2022-10-24 17:29:13 +0800

    fixed shallow copy

--- src/main/java/com/example/jwtdemo/service/CustomUserDetailsService.java
+++ src/main/java/com/example/jwtdemo/service/CustomUserDetailsService.java
@@ -1,7 +1,10 @@
 package com.example.jwtdemo.service;
 
+import com.example.jwtdemo.entity.RoleEntity;
 import com.example.jwtdemo.entity.UserEntity;
+import com.example.jwtdemo.model.RoleDto;
 import com.example.jwtdemo.model.UserDto;
+import com.example.jwtdemo.repository.RoleRepository;
 import com.example.jwtdemo.repository.UserRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +14,18 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -31,19 +41,37 @@ public class CustomUserDetailsService implements UserDetailsService {
         }
 
         UserDto userDto = new UserDto();
-        BeanUtils.copyProperties(user,userDto);
+        BeanUtils.copyProperties(user, userDto);
 
         return userDto;
     }
 
     public UserDto register(UserDto userDto) {
         UserEntity userEntity = new UserEntity();
-        BeanUtils.copyProperties(userDto, userEntity);
+        BeanUtils.copyProperties(userDto, userEntity); // does not do deep copy
+
+        // fetch every role form DB based on role id and set this role to user entity roles
+        Set<RoleEntity> roleSet = userDto.getRoles().stream()
+                .map(RoleDto::getId)
+                .map(id -> roleRepository.findById(id))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toSet());
+
+        userEntity.setRoles(roleSet);
 
         userEntity.setPassword(this.passwordEncoder.encode(userEntity.getPassword()));
         UserEntity savedEntity = userRepository.save(userEntity);
 
         BeanUtils.copyProperties(savedEntity, userDto);
+        Set<RoleDto> roleDtoSet = savedEntity.getRoles().stream().map(roleEntity -> {
+            RoleDto roleDto = new RoleDto();
+            roleDto.setId(roleEntity.getId());
+            roleDto.setRoleName(roleEntity.getRoleName());
+            return roleDto;
+        }).collect(Collectors.toSet());
+        userDto.setRoles(roleDtoSet);
+
         return userDto;
     }
 }
